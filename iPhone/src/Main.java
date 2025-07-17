f8551247ccce06c4cd9793f082430194429fb9ac
Magic302 <higorjulio34@gmail.com>
2025-07-17 20:47:23 -0300

    Browser

--- iPhone/src/Main.java
+++ iPhone/src/Main.java
@@ -8,15 +8,15 @@ public class Main {
 	
 	System.out.println("=====iPhone=====");
 	System.out.println("1 - Selecionar Música");
-	System.out.println("2 - Tocar música");
-	System.out.println("3 - Pausar música");
+	System.out.println("2 - Tocar Música");
+	System.out.println("3 - Pausar Música");
 	System.out.println("4 - Ligar");
 	System.out.println("5 - Atender");
 	System.out.println("6 - Correio de Voz");
-	System.out.println("7 - ");
-	System.out.println("8 - ");
-	System.out.println("9 - ");
-	System.out.println("0 - ");
+	System.out.println("7 - Exibir Página");
+	System.out.println("8 - Nova Aba");
+	System.out.println("9 - Atualizar Página");
+	System.out.println("0 - Sair");
 	
 	while(true) {
 		option = scanner.nextInt();
@@ -37,10 +37,17 @@ public class Main {
 		}
 		case 5 -> iphone.atender();
 		case 6 -> iphone.iniciarCorreioVoz();
+		case 7 -> {
+			System.out.println("Digite o URL");
+			scanner.nextLine();
+			String url = scanner.nextLine();
+			iphone.exibirPagina(url);
+		}
+		case 8 -> iphone.adicionarNovaAba();
+		case 9 -> iphone.atualizarPagina();
 		case 0 -> {scanner.close(); break;}
 		default -> System.out.println("Opção inválida!");
 		}
-		
 	}
 	
 }
--- iPhone/src/iPhone.java
+++ iPhone/src/iPhone.java
@@ -7,17 +7,17 @@ public class iPhone implements MusicPlayer, SmartPhone, Browser{
 	
 	@Override
 	public void exibirPagina(String url) {
-		
+		System.out.println("Exibindo página: " + url);
 	}
 
 	@Override
 	public void adicionarNovaAba() {
-		
+		System.out.println("Nova aba adicionada");
 	}
 
 	@Override
 	public void atualizarPagina() {
-		
+		System.out.println("Página atualizada");
 	}
 
 	
